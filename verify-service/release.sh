b0d3ae5a1df100548ad4bc6b200d9091b478c618
Xavier Lange <xrlange@gmail.com>
2016-05-02 15:55:39 -0700

    What happens when I throw the release script in to travis?

--- verify-service/release.sh
+++ verify-service/release.sh
@@ -5,7 +5,7 @@ DATE_STR=$(date +%Y-%m-%d)
 GITC=$(git rev-parse HEAD)
 TRUNC_GITC=${GITC:35:40}
 RELEASE_NAME=$DATE_STR-$TRUNC_GITC
-# sh build.sh $RELEASE_NAME
+sh build.sh $RELEASE_NAME
 
 pushd artifacts
 for FILE in $(ls); do
