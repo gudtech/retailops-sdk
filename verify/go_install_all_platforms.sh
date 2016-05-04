df2702f20369deea64e1742d9252bc60d741ae5e
Xavier Lange <xrlange@gmail.com>
2016-05-04 16:29:23 -0700

    more renaming

--- verify/go_install_all_platforms.sh
+++ verify/go_install_all_platforms.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
 
-GOOS=windows GOARCH=amd64 go install /go/src/github.com/gudtech/retailops-sdk/verify-service/bin/verify-service.go && mv /artifacts/verify-service.exe /artifacts/verify-windows.exe
-GOOS=linux   GOARCH=amd64 go install /go/src/github.com/gudtech/retailops-sdk/verify-service/bin/verify-service.go && mv /artifacts/verify-service /artifacts/verify-linux
-GOOS=darwin  GOARCH=amd64 go install /go/src/github.com/gudtech/retailops-sdk/verify-service/bin/verify-service.go && mv /artifacts/verify-service /artifacts/verify-darwin
+GOOS=windows GOARCH=amd64 go install /go/src/github.com/gudtech/retailops-sdk/verify/bin/verify.go && mv /artifacts/verify.exe /artifacts/verify-windows.exe
+GOOS=linux   GOARCH=amd64 go install /go/src/github.com/gudtech/retailops-sdk/verify/bin/verify.go && mv /artifacts/verify /artifacts/verify-linux
+GOOS=darwin  GOARCH=amd64 go install /go/src/github.com/gudtech/retailops-sdk/verify/bin/verify.go && mv /artifacts/verify /artifacts/verify-darwin
