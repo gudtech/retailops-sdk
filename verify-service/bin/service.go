481bf4d41f65acce5d42d747801cfd4b85a89e36
Xavier Lange <xrlange@gmail.com>
2016-05-26 17:40:53 -0700

    Making service more robust

--- verify-service/bin/service.go
+++ verify-service/bin/service.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+  "os"
+  "math"
+
   "bytes"
   // "flags"
   "encoding/json"
@@ -12,17 +15,27 @@ type VerifyRequest struct {
   SupportedActions []string `json:"supported_actions"`
 }
 
+type SqrtRequest struct {
+  Input float64 `json:"input"`
+}
+
+type SqrtResponse struct {
+  Output float64 `json:"output"`
+  NaN bool `json:"nan"`
+}
+
 func main() {
   scamp.Initialize("/etc/GTSOA/soa.conf")
 
-  svc,err := scamp.NewService("0.0.0.0:63530","service_verifier")
+  svc,err := scamp.NewService("0.0.0.0:63531","service_verifier")
   if err != nil {
     scamp.Error.Printf("could not create service: `%s`", err.Error())
+    os.Exit(1)
   }
 
   svc.Register("verify", func(msg *scamp.Message, client *scamp.Client) {
     var err error
-    scamp.Error.Printf("received new verify request size: %d", len(msg.Bytes()))
+    // scamp.Error.Printf("received new verify request size: %d", len(msg.Bytes()))
 
     var req VerifyRequest
     err = json.NewDecoder(bytes.NewReader(msg.Bytes())).Decode(&req)
@@ -30,10 +43,47 @@ func main() {
       panic(err.Error())
     }
 
-    scamp.Info.Printf("%s", req)
+    // scamp.Info.Printf("%s", req)
 
     resp := scamp.NewResponseMessage()
     resp.SetRequestId(msg.RequestId)
+    _,err = client.Send(resp)
+    if err != nil {
+      panic(err.Error())
+    }
+  })
+
+
+  svc.Register("sqrt", func(msg *scamp.Message, client *scamp.Client) {
+    var sqrtReq SqrtRequest
+    err = json.NewDecoder(bytes.NewReader(msg.Bytes())).Decode(&sqrtReq)
+    if err != nil {
+      scamp.Error.Printf("nooo: `%s`", err)
+      return
+    }
+
+    // scamp.Info.Printf("sqrt req: %s", sqrtReq)
+
+    var sqrtResp SqrtResponse
+
+    res := math.Sqrt(sqrtReq.Input)
+    if math.IsNaN(res) {
+      sqrtResp.NaN = true
+    } else {
+      sqrtResp.Output = res
+    }
+
+    var buf bytes.Buffer
+    err = json.NewEncoder(&buf).Encode(sqrtResp)
+    if err != nil {
+      scamp.Error.Printf("nooo: `%s`", err)
+      return
+    }
+
+
+    resp := scamp.NewResponseMessage()
+    resp.SetRequestId(msg.RequestId)
+    resp.Write(buf.Bytes())
     _,err = client.Send(resp)
     if err != nil {
       panic(err.Error())
--- verify-service/bin/trigger.go
+++ verify-service/bin/trigger.go
@@ -1,8 +1,13 @@
 package main
 
 import (
-  "time"
+  "sync"
   "github.com/gudtech/scamp-go/scamp"
+  "math/rand"
+  "math"
+
+  "encoding/json"
+  "bytes"
 )
 
 type VerifyRequest struct {
@@ -10,33 +15,73 @@ type VerifyRequest struct {
   SupportedActions []string `json:"supported_actions"`
 }
 
+type SqrtRequest struct {
+  Input float64 `json:"input"`
+}
+
+type SqrtResponse struct {
+  Output float64 `json:"output"`
+  NaN bool `json:"nan"`
+}
+
 func main() {
   scamp.Initialize("/etc/GTSOA/soa.conf")
 
-  client,err := scamp.Dial("0.0.0.0:63530")
-  if err != nil {
-    scamp.Error.Printf("could not dial service: `%s`", err)
-  }
+  var wg sync.WaitGroup
 
-  for i := 0; i < 8000; i++ {
-    reqId := i+1
-    go func() {
-      msg := scamp.NewRequestMessage()
-      msg.SetAction("verify")
-      scamp.Info.Printf("reqId: %d", reqId)
-      msg.SetRequestId(reqId)
-      msg.WriteJson(VerifyRequest {
-        TargetUrl: "http://localhost:9200",
-        SupportedActions: []string { "asdf", "fdas" },
-      })
-      respChan,err := client.Send(msg)
+  for j := 0; j < 2; j++ {
+    wg.Add(1)
+    go func(){
+      client,err := scamp.Dial("0.0.0.0:8080")
       if err != nil {
-        scamp.Error.Printf("could not send message: `%s`", err)
+        scamp.Error.Printf("could not dial service: `%s`", err)
+        return
       }
-      <-respChan
+          scamp.Info.Printf("sup")
+
+      for i := 0; i < 1; i++ {
+        reqId := i+1
+        wg.Add(1)
+        go func() {
+          msg := scamp.NewRequestMessage()
+          msg.SetAction("sqrt")
+          scamp.Info.Printf("reqId: %d", reqId)
+
+          var req SqrtRequest
+          req.Input = rand.NormFloat64()
+
+          expSqrt := math.Sqrt(req.Input)
+
+          var expResp SqrtResponse
+          if math.IsNaN(expSqrt) {
+            expResp.NaN = true
+          } else {
+            expResp.Output = expSqrt
+          }
+          expBytes,err := json.Marshal(expResp)
+          if err != nil {
+            scamp.Info.Printf("could not marshal exp resp: `%s`", err)
+            return
+          }
+
+          msg.SetRequestId(reqId)
+          msg.WriteJson(req)
+          respChan,err := client.Send(msg)
+          if err != nil {
+            scamp.Error.Printf("could not send message: `%s`", err)
+          }
+          resp := <-respChan
+          if !bytes.HasPrefix(resp.Bytes(), expBytes) {
+            scamp.Error.Printf("`%s` vs `%s`", string(resp.Bytes()), string(expBytes))
+            panic("what")
+          }
+          // scamp.Info.Printf("resp: %s", resp.Bytes())
+          wg.Done()
+        }()
+      }
+      wg.Done()
     }()
   }
 
-  time.Sleep(time.Duration(1) * time.Hour)
-
+  wg.Wait()
 }
